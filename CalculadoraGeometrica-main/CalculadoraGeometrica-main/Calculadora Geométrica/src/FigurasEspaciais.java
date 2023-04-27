205b08500a7036a156c8075944c69e88aae09a65
Diego Castan Lopes <127439733+Diegwl@users.noreply.github.com>
2023-04-27 16:53:17 -0300

    Delete CalculadoraGeometrica-main/CalculadoraGeometrica-main/Calculadora Geométrica directory

--- CalculadoraGeometrica-main/CalculadoraGeometrica-main/Calculadora Geométrica/src/FigurasEspaciais.java
+++
@@ -1,7 +0,0 @@
-public abstract class FigurasEspaciais extends FigurasGeometricas{
-    @Override
-    public double calculaArea() {
-        return 0;
-    }
-    public abstract double calculaVolume();
-}
--- CalculadoraGeometrica-main/CalculadoraGeometrica-main/Calculadora Geométrica/src/FigurasGeometricas.java
+++
@@ -1,3 +0,0 @@
-public abstract class FigurasGeometricas {
-    public abstract double calculaArea();
-}
